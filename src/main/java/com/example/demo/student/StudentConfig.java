c9ce8ac70ac917ec5659c8c7d47a9e2001eb7147
Luís Felipe Rocha Martins <luisfelipetochamartins>
2023-06-05 10:05:46 -0300

    Learning SpringBoot

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student luis = new Student(
+                    "Luís Felipe Rocha Martins",
+                    "luisfelipetochamartins@gmail.com",
+                    LocalDate.of(2003, Month.MAY, 27),
+                    20
+            );
+            Student merilo = new Student(
+                    "Merilo algo",
+                    "merilo@gmail.com",
+                    LocalDate.of(2003, Month.AUGUST, 18),
+                    20
+            );
+            repository.saveAll(
+                    List.of(luis, merilo)
+            );
+        };
+    }
+
+}
