621b83e54eedc2c6d212a3053fb27a479a87fa34
Luís Felipe Rocha Martins <luisfelipetochamartins>
2023-06-08 19:17:30 -0300

    Taking a look into PUT and DELETE request

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,8 +22,41 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public void verifyStudentEmail(String email){
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+    }
 
     public void addNewStudent(Student student){
+        verifyStudentEmail(student.getEmail());
+        studentRepository.save(student);
+    }
 
+    public void deleteStudent(Long studentId){
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+            throw new IllegalStateException("Student with id "  + studentId + " does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email){
+        Student student = studentRepository.findById(studentId).
+                orElseThrow(() -> new IllegalArgumentException(
+                        "Student with id " + studentId + " does not exists"
+                ));
+        if(name != null && name.length() > 3 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            verifyStudentEmail(email);
+
+            student.setEmail(email);
+        }
     }
 }
